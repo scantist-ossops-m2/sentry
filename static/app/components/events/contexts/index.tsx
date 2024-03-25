31ee1ca2c883b067466e06a7197d371e828b7d5c
Leander Rodrigues <leander.rodrigues@sentry.io>
2024-03-25 10:20:09 -0700

    ♻️ Ref datasection into its own component

--- static/app/components/events/contexts/index.tsx
+++ static/app/components/events/contexts/index.tsx
@@ -2,11 +2,8 @@ import {Fragment, useCallback, useEffect} from 'react';
 import * as Sentry from '@sentry/react';
 
 import ContextCard from 'sentry/components/events/contexts/contextCard';
-import {CONTEXT_DOCS_LINK} from 'sentry/components/events/contextSummary/utils';
-import {EventDataSection} from 'sentry/components/events/eventDataSection';
+import ContextDataSection from 'sentry/components/events/contexts/contextDataSection';
 import {useHasNewTagsUI} from 'sentry/components/events/eventTags/util';
-import ExternalLink from 'sentry/components/links/externalLink';
-import {t, tct} from 'sentry/locale';
 import type {Group} from 'sentry/types';
 import type {Event} from 'sentry/types/event';
 import {objectIsEmpty} from 'sentry/utils';
@@ -47,30 +44,18 @@ export function EventContexts({event, group}: Props) {
     ];
     // For these context keys, use 'key' as 'type' rather than 'value.type'
     const overrideTypes = new Set(['response', 'feedback', 'user']);
-    return (
-      <EventDataSection
-        key={'context'}
-        type={'context'}
-        title={t('Context')}
-        help={tct(
-          'The structured context items attached to this event. [link:Learn more]',
-          {
-            link: <ExternalLink openInNewTab href={CONTEXT_DOCS_LINK} />,
-          }
-        )}
-      >
-        {orderedContext.map(([k, v]) => (
-          <ContextCard
-            key={k}
-            type={overrideTypes.has(k) ? k : v?.type ?? ''}
-            alias={k}
-            value={v}
-            event={event}
-            group={group}
-          />
-        ))}
-      </EventDataSection>
-    );
+    const cards = orderedContext.map(([k, v]) => (
+      <ContextCard
+        key={k}
+        type={overrideTypes.has(k) ? k : v?.type ?? ''}
+        alias={k}
+        value={v}
+        event={event}
+        group={group}
+      />
+    ));
+
+    return <ContextDataSection cards={cards} />;
   }
 
   return (
