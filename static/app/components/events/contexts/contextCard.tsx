b3a6eee08ea0ffca72dafec1e025e513380b1ade
Leander Rodrigues <leander.rodrigues@sentry.io>
2024-04-05 15:43:23 -0400

    💄 Allow meta to affect context values in cards

--- static/app/components/events/contexts/contextCard.tsx
+++ static/app/components/events/contexts/contextCard.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 
-import {getTitle} from 'sentry/components/events/contexts/utils';
+import {getContextMeta, getContextTitle} from 'sentry/components/events/contexts/utils';
+import {AnnotatedTextErrors} from 'sentry/components/events/meta/annotatedText/annotatedTextErrors';
 import Panel from 'sentry/components/panels/panel';
 import {StructuredData} from 'sentry/components/structuredEventData';
 import {space} from 'sentry/styles/space';
@@ -16,27 +17,31 @@ interface ContextCardProps {
   value?: Record<string, any>;
 }
 
-function ContextCard({alias, type, value = {}}: ContextCardProps) {
+function ContextCard({alias, event, type, value = {}}: ContextCardProps) {
   if (objectIsEmpty(value)) {
     return null;
   }
+  const meta = getContextMeta(event, type);
 
   const content = Object.entries(value).map(([contextKey, contextValue], i) => {
     if (contextKey === 'type') {
       return null;
     }
+    const contextErrors = meta?.[contextKey]?.['']?.err ?? [];
+    const hasErrors = contextErrors.length > 0;
     return (
-      <ContextContent key={i}>
+      <ContextContent key={i} hasErrors={hasErrors}>
         <ContextKey>{contextKey}</ContextKey>
-        <ContextValue>
+        <ContextValue hasErrors={hasErrors}>
           <StructuredData
             value={contextValue}
             withAnnotatedText={false}
             depth={0}
             maxDefaultDepth={0}
-            meta={{}}
+            meta={meta}
             config={{}}
           />
+          <AnnotatedTextErrors errors={contextErrors} />
         </ContextValue>
       </ContextContent>
     );
@@ -44,7 +49,7 @@ function ContextCard({alias, type, value = {}}: ContextCardProps) {
 
   return (
     <Card>
-      <ContextTitle>{getTitle({alias, type, value})}</ContextTitle>
+      <ContextTitle>{getContextTitle({alias, type, value})}</ContextTitle>
       {content}
     </Card>
   );
@@ -66,26 +71,33 @@ const ContextTitle = styled('p')`
   font-weight: bold;
 `;
 
-const ContextContent = styled('div')`
+const ContextContent = styled('div')<{hasErrors: boolean}>`
   display: grid;
   grid-template-columns: subgrid;
   grid-column: span 2;
   padding: ${space(0.25)} ${space(0.75)};
   border-radius: 4px;
+  color: ${p => (p.hasErrors ? p.theme.alert.error.color : p.theme.subText)};
+  border: 1px solid ${p => (p.hasErrors ? p.theme.alert.error.border : 'transparent')};
+  background-color: ${p =>
+    p.hasErrors ? p.theme.alert.error.backgroundLight : p.theme.background};
   &:nth-child(odd) {
-    background-color: ${p => p.theme.backgroundSecondary};
+    background-color: ${p =>
+      p.hasErrors ? p.theme.alert.error.backgroundLight : p.theme.backgroundSecondary};
   }
 `;
 
 const ContextKey = styled('div')`
   grid-column: 1 / 2;
-  color: ${p => p.theme.subText};
   font-family: ${p => p.theme.text.familyMono};
 `;
 
-const ContextValue = styled('div')`
+const ContextValue = styled('div')<{hasErrors: boolean}>`
   grid-column: 2 / 3;
+  color: ${p => (p.hasErrors ? 'inherit' : p.theme.text)};
   font-family: ${p => p.theme.text.familyMono};
+  display: flex;
+  justify-content: space-between;
 `;
 
 export default ContextCard;
