dd7f0f2b8c587c06418c37c8989e240b26d3663f
Tony Xiao <txiao@sentry.io>
2024-04-04 20:43:25 -0400

    feat(trace-explorer): PoC 2 options for trace explorer

--- static/app/views/performance/traces/content.tsx
+++ static/app/views/performance/traces/content.tsx
@@ -1,7 +1,10 @@
-import {useCallback, useMemo} from 'react';
+import {Fragment, useCallback, useMemo, useState} from 'react';
 import {browserHistory} from 'react-router';
 import styled from '@emotion/styled';
 
+import type {GridColumnOrder} from 'sentry/components/gridEditable';
+import GridEditable, {COL_WIDTH_UNDEFINED} from 'sentry/components/gridEditable';
+import SortLink from 'sentry/components/gridEditable/sortLink';
 import * as Layout from 'sentry/components/layouts/thirds';
 import {DatePageFilter} from 'sentry/components/organizations/datePageFilter';
 import {EnvironmentPageFilter} from 'sentry/components/organizations/environmentPageFilter';
@@ -9,21 +12,58 @@ import PageFilterBar from 'sentry/components/organizations/pageFilterBar';
 import {ProjectPageFilter} from 'sentry/components/organizations/projectPageFilter';
 import type {CursorHandler} from 'sentry/components/pagination';
 import Pagination from 'sentry/components/pagination';
+import QuestionTooltip from 'sentry/components/questionTooltip';
+import {SegmentedControl} from 'sentry/components/segmentedControl';
 import type {SmartSearchBarProps} from 'sentry/components/smartSearchBar';
+import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
-import {defined} from 'sentry/utils';
+import type {Sort} from 'sentry/utils/discover/fields';
 import {decodeInteger, decodeScalar} from 'sentry/utils/queryString';
 import {MutableSearch} from 'sentry/utils/tokenizeSearch';
 import {useLocation} from 'sentry/utils/useLocation';
 import {useIndexedSpans} from 'sentry/views/starfish/queries/useIndexedSpans';
 
+import {getFieldRenderer} from './table/fieldRenderers';
 import {fields} from './data';
-import {TraceRow} from './traceRow';
 import {TracesSearchBar} from './tracesSearchBar';
 
 const DEFAULT_PER_PAGE = 20;
 
+type Version = 'v1' | 'v2';
+
 export function Content() {
+  const [version, setVersion] = useState<Version>('v1');
+
+  return (
+    <LayoutMain fullWidth>
+      <PageFilterBar condensed>
+        <ProjectPageFilter />
+        <EnvironmentPageFilter />
+        <DatePageFilter />
+      </PageFilterBar>
+      <SegmentedControl
+        aria-label={t('Trace Explorer Options')}
+        value={version}
+        onChange={v => setVersion(v as Version)}
+      >
+        <SegmentedControl.Item key="v1">
+          {t('Show Individual Spans (Option 1)')}
+        </SegmentedControl.Item>
+        <SegmentedControl.Item key="v2">
+          {t('Show Rollups to Trace/Transaction Root (Option 2)')}
+        </SegmentedControl.Item>
+      </SegmentedControl>
+      {version === 'v1' && <Option1 />}
+      {version === 'v2' && <Option2 />}
+    </LayoutMain>
+  );
+}
+
+type SpanType = 'trace root' | 'transaction root' | 'all';
+
+function Option1() {
+  const [spanType, setSpanType] = useState<SpanType>('trace root');
+
   const location = useLocation();
 
   const query = useMemo(() => {
@@ -55,51 +95,144 @@ export function Content() {
     });
   }, []);
 
-  const filters = useMemo(() => new MutableSearch(query ?? '').filters, [query]);
+  const filters = useMemo(() => {
+    const search = new MutableSearch(query ?? '');
+    if (spanType === 'trace root') {
+      search.addFilterValue('parent_span', '00');
+    } else if (spanType === 'transaction root') {
+      search.addFilterValue('is_segment', '1'); // TODO: this doesnt work yet
+    }
+    return search.filters;
+  }, [spanType, query]);
+
+  const currentSort: Sort = useMemo(() => {
+    const value = decodeScalar(location.query.sort, '');
+    if (!value) {
+      return {field: 'timestamp', kind: 'desc'};
+    }
+    const kind: 'asc' | 'desc' = value[0] === '-' ? 'desc' : 'asc';
+    const field = kind === 'asc' ? value : value.substring(1);
+
+    return {field, kind};
+  }, [location.query]);
+
+  const generateSortLink = useCallback(
+    (field: string) => () => {
+      let sort = `-${field}`;
+      if (currentSort.field === field) {
+        if (currentSort.kind === 'desc') {
+          sort = field;
+        }
+      }
+      return {
+        ...location,
+        query: {
+          ...location.query,
+          sort,
+        },
+      };
+    },
+    [currentSort, location]
+  );
 
   const spansQuery = useIndexedSpans({
     fields,
     filters,
     limit,
-    sorts: [],
+    sorts: [currentSort],
     referrer: 'api.trace-explorer.table',
   });
 
-  const traces = useMemo(() => {
-    const data = (spansQuery.data ?? []).reduce((acc, span) => {
-      const traceId = span.trace;
-      if (!defined(traceId)) {
-        // TODO: warn missing trace id
-        return acc;
-      }
+  const spans = useMemo(() => {
+    return spansQuery.data ?? [];
+  }, [spansQuery]);
 
-      let spansList = acc.get(traceId);
-      if (!defined(spansList)) {
-        spansList = [];
-        acc.set(traceId, spansList);
-      }
-
-      spansList.push(span);
-      return acc;
-    }, new Map());
-
-    return Array.from(data);
-  }, [spansQuery.data]);
+  const columnOrder: GridColumnOrder<any>[] = useMemo(() => {
+    return fields.map(field => {
+      return {
+        key: field,
+        width: COL_WIDTH_UNDEFINED,
+        name: field, // TODO: add more user friendly names for them
+      };
+    });
+  }, []);
 
   return (
-    <LayoutMain fullWidth>
-      <PageFilterBar condensed>
-        <ProjectPageFilter />
-        <EnvironmentPageFilter />
-        <DatePageFilter />
-      </PageFilterBar>
+    <Fragment>
+      <SegmentedControl
+        aria-label={t('Span Type')}
+        value={spanType}
+        onChange={st => setSpanType(st as SpanType)}
+      >
+        <SegmentedControl.Item key="trace root">
+          {t('Search Only Trace Roots')}
+          <QuestionTooltip
+            size="xs"
+            position="top"
+            title={t(
+              'This will only search for spans that have no parent spans. There should be exactly 1 of these per trace.'
+            )}
+          />
+        </SegmentedControl.Item>
+        <SegmentedControl.Item key="transaction root">
+          {t('Search Only Transaction Roots (Includes Trace Roots)')}
+          <QuestionTooltip
+            size="xs"
+            position="top"
+            title={t(
+              'This will only search for spans that are entry points into services. There should be exactly 1 of these per application boundary and the trace root is included.'
+            )}
+          />
+        </SegmentedControl.Item>
+        <SegmentedControl.Item key="all">
+          {t('Show all spans')}
+          <QuestionTooltip
+            size="xs"
+            position="top"
+            title={t('This will search every single span that was sent.')}
+          />
+        </SegmentedControl.Item>
+      </SegmentedControl>
       <TracesSearchBar query={query} handleSearch={handleSearch} />
-      {traces.map(([traceId, spans]) => (
-        <TraceRow key={traceId} traceId={traceId} spans={spans} />
-      ))}
+      <GridEditable
+        isLoading={spansQuery.isFetching}
+        columnOrder={columnOrder}
+        columnSortBy={[]}
+        data={spans}
+        grid={{
+          renderHeadCell: renderHeadCell(generateSortLink, currentSort),
+          renderBodyCell: renderBodyCell(),
+        }}
+        location={location}
+      />
       <StyledPagination pageLinks={spansQuery.pageLinks} onCursor={handleCursor} />
-    </LayoutMain>
+    </Fragment>
   );
+}
+
+function renderHeadCell(generateSortLink, currentSort) {
+  return function (col: GridColumnOrder<any>) {
+    return (
+      <SortLink
+        align="left"
+        canSort
+        direction={col.key === currentSort.field ? currentSort.kind : undefined}
+        generateSortLink={generateSortLink(col.key)}
+        title={col.name}
+      />
+    );
+  };
+}
+
+function renderBodyCell() {
+  return function (column: GridColumnOrder<any>, row: any) {
+    const Renderer = getFieldRenderer(column.key);
+    return <Renderer column={column} row={row} />;
+  };
+}
+
+function Option2() {
+  return <Fragment />;
 }
 
 const LayoutMain = styled(Layout.Main)`
--- static/app/views/performance/traces/table/fieldRenderers.tsx
+++ static/app/views/performance/traces/table/fieldRenderers.tsx
@@ -1,16 +1,157 @@
+import type {FC} from 'react';
+import {Fragment} from 'react';
+
+import type {GridColumnOrder} from 'sentry/components/gridEditable';
 import ProjectBadge from 'sentry/components/idBadge/projectBadge';
 import Link from 'sentry/components/links/link';
+import PerformanceDuration from 'sentry/components/performanceDuration';
 import type {DateString} from 'sentry/types';
-import {Container} from 'sentry/utils/discover/styles';
+import {defined} from 'sentry/utils';
+import {Container, FieldDateTime} from 'sentry/utils/discover/styles';
 import {getShortEventId} from 'sentry/utils/events';
 import {getTransactionDetailsUrl} from 'sentry/utils/performance/urls';
 import Projects from 'sentry/utils/projects';
+import {decodeScalar} from 'sentry/utils/queryString';
 import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
 import usePageFilters from 'sentry/utils/usePageFilters';
 import useProjects from 'sentry/utils/useProjects';
 import {getTraceDetailsUrl} from 'sentry/views/performance/traceDetails/utils';
 import {transactionSummaryRouteWithQuery} from 'sentry/views/performance/transactionSummary/utils';
+
+export function getFieldRenderer(field: string): FC<FieldRendererProps> {
+  return fieldRenderers[field] ?? DefaultRenderer;
+}
+
+const fieldRenderers = {
+  project: ProjectFieldRenderer,
+  span_id: SpanIdFieldRenderer,
+  trace: TraceIdFieldRenderer,
+  'transaction.id': TransactionIdFieldRenderer,
+  transaction: TransactionFieldRenderer,
+  timestamp: TimestampFieldRenderer,
+  'span.duration': SpanDurationFieldRenderer,
+  'span.self_time': SpanSelfTimeFieldRenderer,
+};
+
+interface FieldRendererProps {
+  column: GridColumnOrder<any>;
+  row: any;
+}
+
+function DefaultRenderer({row, column}: FieldRendererProps) {
+  // TODO: this can be smarter based on the type of the value
+  return <Container>{row[column.key]}</Container>;
+}
+
+function ProjectFieldRenderer(props: FieldRendererProps) {
+  const projectSlug = props.row.project;
+
+  if (!defined(projectSlug)) {
+    return <DefaultRenderer {...props} />;
+  }
+
+  return <ProjectRenderer {...props} projectSlug={projectSlug} />;
+}
+
+function SpanIdFieldRenderer(props: FieldRendererProps) {
+  const projectSlug = props.row.project;
+  const spanId = props.row.span_id;
+  const transactionId = props.row['transaction.id'];
+
+  if (!defined(projectSlug) || !defined(spanId)) {
+    return <DefaultRenderer {...props} />;
+  }
+
+  return (
+    <SpanIdRenderer
+      {...props}
+      projectSlug={projectSlug}
+      spanId={spanId}
+      transactionId={transactionId}
+    />
+  );
+}
+
+function TraceIdFieldRenderer(props: FieldRendererProps) {
+  const traceId = props.row.trace;
+
+  if (!defined(traceId)) {
+    return <DefaultRenderer {...props} />;
+  }
+
+  return (
+    <TraceIdRenderer
+      {...props}
+      traceId={traceId}
+      transactionId={props.row['transaction.id'] ?? undefined}
+      timestamp={props.row.timestamp}
+    />
+  );
+}
+
+function TransactionIdFieldRenderer(props: FieldRendererProps) {
+  const projectSlug = props.row.project;
+  const transactionId = props.row['transaction.id'];
+
+  if (!defined(projectSlug) || !defined(transactionId)) {
+    return <DefaultRenderer {...props} />;
+  }
+
+  return (
+    <TransactionIdRenderer
+      {...props}
+      projectSlug={projectSlug}
+      transactionId={transactionId}
+    />
+  );
+}
+
+function TransactionFieldRenderer(props: FieldRendererProps) {
+  const projectSlug = props.row.project;
+  const transaction = props.row.transaction;
+
+  if (!defined(projectSlug) || !defined(transaction)) {
+    return <DefaultRenderer {...props} />;
+  }
+
+  return (
+    <TransactionRenderer {...props} projectSlug={projectSlug} transaction={transaction} />
+  );
+}
+
+function TimestampFieldRenderer(props: FieldRendererProps) {
+  const location = useLocation();
+  const timestamp = props.row.timestamp;
+
+  if (!defined(timestamp)) {
+    return <DefaultRenderer {...props} />;
+  }
+
+  const utc = decodeScalar(location?.query?.utc) === 'true';
+
+  return <FieldDateTime date={timestamp} year seconds timeZone utc={utc} />;
+}
+
+function SpanDurationFieldRenderer(props: FieldRendererProps) {
+  const duration = props.row['span.duration'];
+
+  if (!defined(duration)) {
+    return <DefaultRenderer {...props} />;
+  }
+
+  return <PerformanceDuration milliseconds={duration} abbreviation />;
+}
+
+function SpanSelfTimeFieldRenderer(props: FieldRendererProps) {
+  const duration = props.row['span.self_time'];
+
+  if (!defined(duration)) {
+    return <DefaultRenderer {...props} />;
+  }
+
+  return <PerformanceDuration milliseconds={duration} abbreviation />;
+}
 
 interface ProjectRendererProps {
   projectSlug: string;
@@ -39,7 +180,7 @@ export function ProjectRenderer({projectSlug}: ProjectRendererProps) {
 interface SpanIdRendererProps {
   projectSlug: string;
   spanId: string;
-  transactionId: string;
+  transactionId?: string;
 }
 
 export function SpanIdRenderer({
@@ -48,6 +189,10 @@ export function SpanIdRenderer({
   transactionId,
 }: SpanIdRendererProps) {
   const organization = useOrganization();
+
+  if (!defined(transactionId)) {
+    return <Fragment>{getShortEventId(spanId)}</Fragment>;
+  }
 
   const target = getTransactionDetailsUrl(
     organization.slug,
